9718c152043256416b82c8f984e94cda01c4dc2a
Natiree <natireeritan@gmail.com>
2026-05-09 20:04:01 +0300

    Refactor Australia map coloring code
    Reformatted code for Australia map coloring problem.

--- task2a_australiaColouring.py
+++ task2a_australiaColouring.py
@@ -2,17 +2,13 @@ from constraint import Problem
 
 problem = Problem()
 
-# Regions
 regions = ["WA", "NT", "SA", "Q", "NSW"]
 
-# Colors
 colors = ["Red", "Green", "Blue"]
 
-# Add variables
 for region in regions:
     problem.addVariable(region, colors)
 
-# Constraints
 problem.addConstraint(lambda a, b: a != b, ("WA", "NT"))
 problem.addConstraint(lambda a, b: a != b, ("WA", "SA"))
 problem.addConstraint(lambda a, b: a != b, ("NT", "SA"))
@@ -21,7 +17,6 @@ problem.addConstraint(lambda a, b: a != b, ("SA", "Q"))
 problem.addConstraint(lambda a, b: a != b, ("SA", "NSW"))
 problem.addConstraint(lambda a, b: a != b, ("Q", "NSW"))
 
-# Get solution
 solution = problem.getSolution()
 
 print("Australia Map Coloring:")
